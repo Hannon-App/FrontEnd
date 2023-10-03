9a522952ca664b045862bba0b06a81222ff22fca
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-10-03 14:58:47 +0700

    added landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 // import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LoginAdmin from "./pages/LoginAdmin/LoginAdmin";
+import LandingPage from "./pages/LandingPage/Index";
+import LoginAdmin from "./pages/Login/LoginAdmin";
 import AdminDashboard from "./pages/AdminDashboard/Index";
 
 const App = () => {
+  
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/AdminDashboard" element={<AdminDashboard/>} />
+          <Route path="/" element={<LandingPage/>} />
+          <Route path="/AdminDashboard" element={<AdminDashboard />} />
           <Route path="/LoginAdmin" element={<LoginAdmin />} />
         </Routes>
       </BrowserRouter>
---
+++ src/layout/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className="bg-blue-700 p-4 m-3 rounded-md">
+            <div className="container mx-auto">
+                <div className="flex items-center justify-between">
+                    <div className="text-white font-semibold text-xl">Immersive Dashboard</div>
+                    <div className="md:hidden">
+                        {/* Tombol Hamburger untuk tampilan mobile */}
+                        <button className="text-white">
+                            <i className="fas fa-bars"></i>
+                        </button>
+                    </div>
+                    <div className="">
+                        {/* Tautan-tautan menu */}
+                        {/* <Dropdown/> */}
+
+                    </div>
+                    
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
