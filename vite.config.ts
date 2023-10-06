f5e70213ed05bd45cc3651eabb1add48893a5049
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-10-06 13:44:47 +0700

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Batas ukuran chunk sebelum peringatan
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
+
